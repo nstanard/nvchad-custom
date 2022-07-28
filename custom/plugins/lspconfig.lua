b2f125a18d0207fac73c715b47ae05ba530b8ecf
Neal Stanard <nstanard@gmail.com>
2022-07-28 14:44:26 -0500

    add tsserver for TS file linting

--- custom/plugins/lspconfig.lua
+++ custom/plugins/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "tsserver" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
