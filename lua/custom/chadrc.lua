0f505bbda94890dbcba7bd6be351332624e40acf
Neal Stanard <nstanard@users.noreply.github.com>
2022-07-28 08:09:17 -0500

    Update chadrc.lua

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,12 +1,15 @@
--- Just an example, supposed to be placed in /lua/custom/
-
 local M = {}
 
--- make sure you maintain the structure of `core/default_config.lua` here,
--- example of changing theme:
+M.plugins = {
+   status = {
+      colorizer = true,
+   },
+}
 
 M.ui = {
-  theme = "tomorrow_night",
+   theme = "tomorrow_night",
 }
+
+M.mappings = require "custom.mappings"
 
 return M
