a40d360d9e0c11cc774e74b9e1c7600528243eba
Neal Stanard <nstanard@gmail.com>
2022-08-02 10:55:19 -0500

    formatting

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -4,18 +4,18 @@ M.general = {
 	n = {
 		["<leader>q"] = { ":q! <CR>", "  exit nvchad" },
 		["<leader>te"] = { ":Telescope <CR>", "  open telescope options" },
-    ["<leader>mt"] = {
-      function()
-        require("nvterm.terminal").send("npm run test", "float")
-      end,
-      "   run tests",
-    },
-    ["<leader>mi"] = {
-      function()
-        require("nvterm.terminal").send("npm install", "float")
-      end,
-      "   run tests",
-    },
+		["<leader>mt"] = {
+			function()
+				require("nvterm.terminal").send("npm run test", "float")
+			end,
+			"   run tests",
+		},
+		["<leader>mi"] = {
+			function()
+				require("nvterm.terminal").send("npm install", "float")
+			end,
+			"   run tests",
+		},
 	},
 }
 
